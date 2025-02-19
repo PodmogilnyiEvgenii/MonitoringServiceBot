e5abc3357374f97ae47b3b899565b026926b7e66
PodmogilnyiEvgenii <evgenij105@mail.ru>
2025-02-19 11:39:15 +0300

    Initial commit

---
+++ src/main/java/com/home/monitoringservicebot/MonitoringServiceBotApplication.java
@@ -0,0 +1,13 @@
+package com.home.monitoringservicebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringServiceBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringServiceBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/monitoringservicebot/MonitoringServiceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.monitoringservicebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringServiceBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
